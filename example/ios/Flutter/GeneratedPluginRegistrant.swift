689f8f6bb5ab8403f2e89a57fc0c8860348552f2
Swanav <swanav@outlook.com>
2021-10-24 03:29:48 +0530

    Added support for macos

---
+++ example/ios/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_micro_ecc
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMicroEccPlugin.register(with: registry.registrar(forPlugin: "FlutterMicroEccPlugin"))
+}
